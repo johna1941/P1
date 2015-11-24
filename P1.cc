164b400de0946383a45e7537f6ad7562ffd4ad80
PDMPhysSC <samuel.clarke@student.manchester.ac.uk>
2015-11-24 15:02:56 +0000

    Edited the initialising macro files to make more logical sense (ie. vis.mac no longer initialises the gun; init_run.mac does).

--- P1.cc
+++ P1.cc
@@ -116,7 +116,8 @@ int main(int argc,char** argv)
   }
   else { 
     // interactive mode
-    UImanager->ApplyCommand("/control/execute init_vis.mac");
+   	UImanager->ApplyCommand("/control/execute init_vis.mac");
+	UImanager->ApplyCommand("/control/execute init_run.mac");
 	if (ui->IsGUI()) {
 		UImanager->ApplyCommand("/control/execute gui.mac");
 	}
