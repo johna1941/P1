4d80048688c1c9b6645edfff52d31b4015046deb
PDMPhysSC <samuel.clarke@student.manchester.ac.uk>
2015-12-10 11:52:13 +0000

    Comment, JA: how do we add Fibre 3 and 4 to this?

--- src/P1SensitiveDetector.cc
+++ src/P1SensitiveDetector.cc
@@ -55,7 +55,7 @@ G4bool P1SensitiveDetector::ProcessHits(G4Step* step,
   } else {
     axis = p1dc->fFibre2_axis;
   }
-
+// Need to somehow add Fibre3 and 4 to this 
   G4ThreeVector direction = track->GetMomentumDirection();
   if (direction * axis < 0.995) {
     // Too far from axis - don't count?
