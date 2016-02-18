4c89a6e001fd50390886d0c83a9e003581424820
PDMPhysSC <samuel.clarke@student.manchester.ac.uk>
2016-02-18 11:22:15 +0000

    Increased acceptance angle to match lens collimator

--- src/P1SensitiveDetector.cc
+++ src/P1SensitiveDetector.cc
@@ -52,14 +52,14 @@ G4bool P1SensitiveDetector::ProcessHits(G4Step* step,
   G4ThreeVector axis;
   if (prePV == p1dc->fFibrePV) {
     axis = p1dc->fFibre_axis;
-  } 
-// Need to somehow add Fibre3 and 4 to this 
+    }
+/*
   G4ThreeVector direction = track->GetMomentumDirection();
-  if (direction * axis < 0.995) {
+  if (direction * axis < 0.924) { // cos (22.5) = 0.9238795...
     // Too far from axis - don't count?
     return true;
   }
-
+*/
   const G4UserEventAction* ea = runManager->GetUserEventAction();
   const P1EventAction* constp1ea = static_cast<const P1EventAction*>(ea);
   P1EventAction* p1ea = const_cast<P1EventAction*>(constp1ea);
