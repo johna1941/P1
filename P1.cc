f9879f90ee20be873cd8fc9ce64bda0021727519
PDMPhysSC <samuel.clarke@student.manchester.ac.uk>
2016-02-23 14:11:20 +0000

    Optimised for batch running

--- P1.cc
+++ P1.cc
@@ -76,7 +76,7 @@ int main(int argc,char** argv)
   //
 #ifdef G4MULTITHREADED
   G4MTRunManager* runManager = new G4MTRunManager;
-  runManager->SetNumberOfThreads(8);
+  runManager->SetNumberOfThreads(4);
 #else
   G4RunManager* runManager = new G4RunManager;
 #endif
